b93f681167d2b9b1a41e292a1eb5b2f77ff58a55
Li1412 <124526316+Li1412@users.noreply.github.com>
2023-04-02 21:55:24 +0200

    new apikey generated

--- app.js
+++ app.js
@@ -3,37 +3,41 @@ function formatDate(timestamp) {
     let hours = date.getHours();
     if (hours < 10) {
     hours = `0${hours}`;
+
     }
-        
+
     let minutes = date.getMinutes();
     if (minutes < 10) {
     minutes = `0${minutes}`;
+
     }
-        
+
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
     }
-    
+
     function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    }
     
+    return days[day];
+}
     function displayForecast(response) {
-    let forecast = response.data.daily;
-    let forecastElement = document.querySelector("#forecast");
+        let forecast = response.data.daily;
+    
+        let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
-    forecast.forEach(function(forecastDay, index) {
+    forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
     forecastHTML = forecastHTML +
     `
     <div class="col-2">
     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
     <img 
-    src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+    src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
     alt=""
     width="42"
     />
@@ -51,7 +55,7 @@ function formatDate(timestamp) {
     }
     
     function getForecast(coordinates) {
-    let apiKey = "4bb9139b18c97fe29a828148b";
+    let apiKey = "9cb72bec958f8fb02391985ed7b219d2";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
     }
@@ -71,16 +75,16 @@ function formatDate(timestamp) {
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
+    windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
     
-getForecast(response.data.coord);
+    getForecast(response.data.coord);
     }
     
     function search(city) {
-    let apiKey = "4bb9139b18c97fe29a8281483d01255b";
+    let apiKey = "9cb72bec958f8fb02391985ed7b219d2"
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
     }
@@ -94,6 +98,5 @@ getForecast(response.data.coord);
     let form = document.querySelector("#search-form");
     form.addEventListener("submit", handleSubmit);
     
+    let celsiusTemperature = 0;
     search("Linden");
-    
-    
